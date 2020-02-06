090888a33c5684150b00b00e519974a7fba69f4b
cassandra444 <guittard.cassandra@gmail.com>
2020-02-06 08:38:33 +0100

    Update JDVELH.C

--- JDVELH.C
+++ JDVELH.C
@@ -13,30 +13,7 @@ int main() {
 	
 	typedef struct Objet Objet ;
 	
-	Objet tunique = {5, "tunique"};
-	Objet dague = {8, "dague"};
-	Objet epee = {10, "epee"};
-	Objet bouclier = {15, "bouclier"};
-	Objet bottes = {5, "bottes"};
-	
-	
-	
-	/*
-	printf ("inventaire : \n");
-	
-	printf ("-%s", tunique.nom );
-	printf (" / %d pieces \n",tunique.prix);
-	printf ("-%s", dague.nom );
-	printf (" / %d pieces \n",dague.prix);
-	
-	printf ("-%s", epee.nom );
-	printf (" / %d pieces \n",epee.prix);
-	printf ("-%s", bouclier.nom );
-	printf (" / %d pieces \n",bouclier.prix);
-	printf ("-%s", bottes.nom );
-	printf (" / %d pieces \n",bottes.prix);
-	*/
-	
+
 }
 
 
