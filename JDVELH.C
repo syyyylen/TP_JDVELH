de3930c2e185226b73dfa30a4e800d9deb264967
Jules Vannier <julesvannier44@gmail.com>
2020-02-06 11:16:51 +0100

    Update JDVELH.C

--- JDVELH.C
+++ JDVELH.C
@@ -10,17 +10,16 @@ int position = 0;
 		int gold ;
 		int pv ;
 		char description[150];
-		//int id[10] ;
 		int numero;
-		
+		int id[2] ;
 	
 	};
 	
 	typedef struct Chapitre Chapitre ;
 	
-	Chapitre chapitre1 = {10, 3,"ceci est la description du premier chapitre",1};
-	Chapitre chapitre2 = {15, 5,"ceci est la description du second chapitre",2};
-	Chapitre chapitre3 = {20, 7,"ceci est la description du troisieme chapitre",3};
+	Chapitre chapitre1 = {10, 3,"ceci est la description du premier chapitre",1,{2,3}};
+	Chapitre chapitre2 = {15, 5,"ceci est la description du second chapitre",2,{3,1}};
+	Chapitre chapitre3 = {20, 7,"ceci est la description du troisieme chapitre",3,{1,2}};
 
 int deplacement() {
 		
@@ -30,22 +29,9 @@ int deplacement() {
 		printf("%d",chapitre1.gold);
 		printf("%d",position);*/
 
+		
 		printf("Choisissez dans quel chapitre aller : \n");
 		
-		if(position = 1){
-			printf("-tapez 1 pour aller dans le chapitre 1 \n tapez 2 pour aller dans le chapitre 2 \n tapez 3 pour aller dans le chapitre 3 \n"); 
-		}
-		else if(position = 2){
-					printf("-tapez 0 pour aller dans le chapitre 0 \n tapez 2 pour aller dans le chapitre 2 \n tapez 3 pour aller dans le chapitre 3 \n"); 
-		}
-		else if(position = 3){
-					printf("-tapez 0 pour aller dans le chapitre 0 \n tapez 1 pour aller dans le chapitre 1 \n tapez 3 pour aller dans le chapitre 3 \n"); 
-		}
-		else if(position = 4){
-					printf("-tapez 0 pour aller dans le chapitre 0 \n tapez 1 pour aller dans le chapitre 1 \n tapez 2 pour aller dans le chapitre 2 \n"); 
-		}
-		
-		scanf("%d",retour); 
 		
 		return retour ;
 	}
@@ -57,28 +43,6 @@ int main() {
 	printf("bienvenue dans le jeu dont vous etes le heros \n");
 
 	move = deplacement() ;
-	
-	if(move = 1){
-			printf("bienvenue dans le jeu dont vous etes le heros \n"); 
-			move = deplacement() ;
-		}
-		else if(move = 2){
-					printf("%n \n",chapitre1.description); 
-					move = deplacement() ;
-		}
-		else if(move = 3){
-					printf("%n \n",chapitre3.description); 
-					move = deplacement() ;
-		}
-		else if(move = 4){
-					printf("%n \n",chapitre3.description); 
-					move = deplacement() ;
-		}
-	
-	
-	
 
-	
-	
 }
 
