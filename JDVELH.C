67fa668ed92a21849feac8acdd777a5ece337337
Jules Vannier <julesvannier44@gmail.com>
2020-02-06 09:59:07 +0100

    Structures lieux completes
    attention tableau d'id

--- JDVELH.C
+++ JDVELH.C
@@ -7,10 +7,11 @@ int main() {
 	
 	struct Chapitre {
 
-		int or ;
-		int pv;
-		char description [100] ;
-		int id[10] ;
+		int gold ;
+		int pv ;
+		char description[150];
+		//int id[10] ;
+		int numero;
 		
 	
 	};
@@ -20,10 +21,11 @@ int main() {
 	Chapitre chapitre1 = {10, 3,"ceci est la description du premier chapitre",1};
 	Chapitre chapitre2 = {15, 5,"ceci est la description du second chapitre",2};
 	Chapitre chapitre3 = {20, 7,"ceci est la description du troisieme chapitre",3};
+
 	
-	//printf("test");
-	
-	/*printf ("Entrez 1 pour aller a premiere destination\n entrez 2 pour aller a deuxieme destination"); 
+	/*int deplacement() {
+		
+		printf ("Entrez 1 pour aller a premiere destination \n entrez 2 pour aller a deuxieme destination"); 
 	scanf("%d",&choix);
 	
 	if(choix==1){
@@ -32,13 +34,16 @@ int main() {
 	else if(choix==2) {
 		printf ("vous etes arrive a la deuxieme destination");
 		
-	}*/
-
-	int deplacement() {
-		
-		
-		
 		return 0 ; 
+	}*/
+	
+	//printf ("salut %d",chapitre1.numero);
+
+	int deplacement(){
+		
+		
+		
+		return 0 ;
 	}
 	
 	
