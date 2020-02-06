7a0a1d386c8198e36beecd43ed4c3d83249a8d33
Jules Vannier <julesvannier44@gmail.com>
2020-02-06 10:56:14 +0100

    Deplacements

--- JDVELH.C
+++ JDVELH.C
@@ -3,7 +3,7 @@
 #include <time.h>
 
 
-int position = 1; 
+int position = 0; 
 	
 	struct Chapitre {
 
@@ -24,21 +24,60 @@ int position = 1;
 
 int deplacement() {
 		
+		int retour;
+		
 		/*printf("la fonction demarre et elle connait :");
 		printf("%d",chapitre1.gold);
 		printf("%d",position);*/
 
 		printf("Choisissez dans quel chapitre aller : \n");
 		
+		if(position = 1){
+			printf("-tapez 1 pour aller dans le chapitre 1 \n tapez 2 pour aller dans le chapitre 2 \n tapez 3 pour aller dans le chapitre 3 \n"); 
+		}
+		else if(position = 2){
+					printf("-tapez 0 pour aller dans le chapitre 0 \n tapez 2 pour aller dans le chapitre 2 \n tapez 3 pour aller dans le chapitre 3 \n"); 
+		}
+		else if(position = 3){
+					printf("-tapez 0 pour aller dans le chapitre 0 \n tapez 1 pour aller dans le chapitre 1 \n tapez 3 pour aller dans le chapitre 3 \n"); 
+		}
+		else if(position = 4){
+					printf("-tapez 0 pour aller dans le chapitre 0 \n tapez 1 pour aller dans le chapitre 1 \n tapez 2 pour aller dans le chapitre 2 \n"); 
+		}
 		
-		return 0 ;
+		scanf("%d",retour); 
+		
+		return retour ;
 	}
 
 int main() {
 	
-	int move ;
+	int move ; 
+	
+	printf("bienvenue dans le jeu dont vous etes le heros \n");
 
-	move = deplacement() ; 
+	move = deplacement() ;
+	
+	if(move = 1){
+			printf("bienvenue dans le jeu dont vous etes le heros \n"); 
+			move = deplacement() ;
+		}
+		else if(move = 2){
+					printf("%n \n",chapitre1.description); 
+					move = deplacement() ;
+		}
+		else if(move = 3){
+					printf("%n \n",chapitre3.description); 
+					move = deplacement() ;
+		}
+		else if(move = 4){
+					printf("%n \n",chapitre3.description); 
+					move = deplacement() ;
+		}
+	
+	
+	
+
 	
 	
 }
