ac02c5d4e8db66c469cfb10c0097f42be42c6fcb
Jules Vannier <julesvannier44@gmail.com>
2020-02-06 09:22:52 +0100

    Update JDVELH.C

--- JDVELH.C
+++ JDVELH.C
@@ -4,39 +4,20 @@
 
 int main() {
 	
-	struct Objet {
+	
+	//printf("test");
+	
+	printf ("Entrez 1 pour aller a premiere destination\n entrez 2 pour aller a deuxieme destination"); 
+	scanf("%d",&choix);
+	
+	if(choix==1){
+		printf ("vous etes arrive a la premiere destination");
+	}
+	else if(choix==2) {
+		printf ("vous etes arrive a la deuxieme destination");
+		
+	}
 
-		int prix ;
-		char nom[20] ;
-	
-	};
-	
-	typedef struct Objet Objet ;
-	
-	Objet tunique = {5, "tunique"};
-	Objet dague = {8, "dague"};
-	Objet epee = {10, "epee"};
-	Objet bouclier = {15, "bouclier"};
-	Objet bottes = {5, "bottes"};
-	
-	
-	
-	/*
-	printf ("inventaire : \n");
-	
-	printf ("-%s", tunique.nom );
-	printf (" / %d pieces \n",tunique.prix);
-	printf ("-%s", dague.nom );
-	printf (" / %d pieces \n",dague.prix);
-	
-	printf ("-%s", epee.nom );
-	printf (" / %d pieces \n",epee.prix);
-	printf ("-%s", bouclier.nom );
-	printf (" / %d pieces \n",bouclier.prix);
-	printf ("-%s", bottes.nom );
-	printf (" / %d pieces \n",bottes.prix);
-	*/
-	
 }
 
 
